6b2260f30ab8cd634cbcfcf17880390b47264462
supersfel <jmg081005@naver.com>
2023-01-31 10:28:27 +0900

    feat(fe): sticky 애니메이션 구현

---
+++ fe/src/components/introduce/Intro_2.tsx
@@ -0,0 +1,43 @@
+/*  */
+import React from 'react';
+
+const Intro_2 = () => {
+  return (
+    <div className="intro_2">
+      <div className="description-area">
+        <div className="text-wrapper">
+          <p className="title">
+            <span>팀원</span>들과 함께
+          </p>
+          <div className="description">
+            <p>가말다는 팀원 공동 작업이 가능합니다</p>
+            <p>실시간으로 팀원들과 일정을 관리해보세요</p>
+          </div>
+        </div>
+
+        <div className="text-wrapper">
+          <p className="title">
+            보다 <span>손쉽게</span>
+          </p>
+          <div className="description">
+            <p>가말다는 쉬운 조작을 생각합니다</p>
+            <p>직관적인 방법으로 마일스톤을 만들어 보세요</p>
+          </div>
+        </div>
+        <div className="text-wrapper">
+          <p className="title">
+            <span>왜</span> 가말다인가?
+          </p>
+          <div className="description">
+            <p>직관적인 프로젝트 마일스톤을 원한다면</p>
+            <p>가말다가 정답입니다.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="img-box"></div>
+    </div>
+  );
+};
+
+export default Intro_2;
--- fe/src/pages/Introduce.tsx
+++ fe/src/pages/Introduce.tsx
@@ -1,11 +1,13 @@
 /*  */
 import Intro_1 from 'components/introduce/Intro_1';
+import Intro_2 from 'components/introduce/Intro_2';
 import React from 'react';
 
 const Introduce = () => {
   return (
     <>
       <Intro_1 />
+      <Intro_2 />
     </>
   );
 };
