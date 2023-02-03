d78053513e955a846535d1cd5543cb0d37978686
supersfel <jmg081005@naver.com>
2023-02-03 18:34:13 +0900

    feat(fe): 토스트 적용

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -4,12 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import Introduce from 'pages/Introduce';
 
 import 'styles/main.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import Toast from 'components/modules/Toast/Toast';
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Introduce />}></Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="" element={<Introduce />}></Route>
+      </Routes>
+      <Toast />
+    </>
   );
 }
 
---
+++ fe/src/components/modules/Toast/Toast.tsx
@@ -0,0 +1,33 @@
+/* 토스트 모듈 관련 세팅 */
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+const Toast = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
+
+//사용법
+/* 
+alert대신 아래 문구를 선택하면 된다
+
+toast.success("문구")
+toast.error("문구")
+toast.info("문구")
+toast.warning("문구")
+toast("문구")
+*/
+
+export default Toast;
