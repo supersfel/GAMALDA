5e91df03d0ff16905d94d1900c55bd0cc0ea746a
Gotkwondo <dhwngus99@gmail.com>
2023-02-07 21:50:53 +0900

    feat(fe): 네이버 로그인 api 연결

--- fe/src/components/login/Naver_Login.tsx
+++ fe/src/components/login/Naver_Login.tsx
@@ -2,6 +2,8 @@ import { ReactComponent as GamaldaSVG } from 'assets/svg/gamaldalogo.svg';
 import naverLogin from 'assets/png/naver_login.png';
 
 const Nav_Login = () => {
+  //  process.env.REACT_APP_NAVER_CLIENT_ID 는 네이버에서 생성된 client ID, process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL 는 네이버에서 설정한 콜백 url이다.
+  let naver_api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${encodeURI(process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL!)}&state=${Math.random().toString(36).substr(3, 14)}`;
   return (
     <div className="login_box">
       <div className="title_area flex_center">
@@ -11,10 +13,9 @@ const Nav_Login = () => {
         <GamaldaSVG width="220px" height="220px"/>
       </div>
       <div className="login_button_area flex_center">
-        <button>
+        <button onClick={() =>window.open(naver_api_url,'Naver Login','width=430,height=500,location=no,status=no,scrollbars=yes')}>
           <img className="login_button_img" src={naverLogin} alt="네이버 로그인"/>
         </button>
-        
       </div>
     </div>
   )
