f9344cd90dadd44c531ca0b593acfcb9970cd4b1
supersfel <jmg081005@naver.com>
2023-02-09 21:06:33 +0900

    feat(be,fe): socket.io 연결

--- fe/src/pages/Milestone.tsx
+++ fe/src/pages/Milestone.tsx
@@ -1,20 +1,25 @@
 /* 마엘스톤 페이지 */
 import { getTest } from 'api/project/api';
+import MilestoneBasic from 'components/milestone/MilestoneBasic';
 import Header from 'components/modules/Header/Header';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Milestone = () => {
-  const test = async () => {
-    const res = await getTest();
-    console.log(res);
-  };
+  const projectId = useParams().projectId as string;
+
+  // const test = async () => {
+  //   const res = await getTest();
+  //   console.log(res);
+  // };
 
   return (
     <>
       <Header authorized={false} userName={'오주현'} />
-      <button onClick={test} style={{ margin: 100 }}>
+      {/* <button onClick={test} style={{ margin: 100 }}>
         테스트
-      </button>
+      </button> */}
+      <MilestoneBasic projectId={projectId} />
     </>
   );
 };
