d2fea67e71e3a4dd835b88274731cfd5e2a2d7fe
Gotkwondo <dhwngus99@gmail.com>
2023-04-07 17:45:30 +0900

    refactor(be): 로그아웃 동작 API 사용

--- fe/src/pages/LogoutWork.tsx
+++ fe/src/pages/LogoutWork.tsx
@@ -1,17 +1,11 @@
+import { logout } from 'api/logout/api';
 import axios from 'axios';
 import { useEffect } from 'react';
 
 const LogoutWork = () => {
   //  로그아웃 GET메서드 사용을 위한 페이지
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MAIN_URL}/naver_login/logout`)
-      .then((res) => {
-        console.log(res.data)
-        if (res.data === 'cookieDeleted') {
-          console.log('쿠키 삭제됨');
-          window.location.href = `${process.env.REACT_APP_MAIN_URL}`;
-        }
-    })
+    logout()
   })
   return (
     <> </>
