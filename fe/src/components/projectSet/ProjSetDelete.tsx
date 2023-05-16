f75095f7e88f138565e6dc82b33d47a88ae2b4c2
정민규 <jmg081005@naver.com>
2023-05-16 17:58:36 +0900

    feat(fe): delete 퍼블리싱

--- fe/src/components/projectSet/ProjSetDelete.tsx
+++ fe/src/components/projectSet/ProjSetDelete.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const ProjSetDelete = () => {
-  return <div>delte</div>;
+  return (
+    <div className="proj-set-delete">
+      <p>프로젝트 삭제</p>
+      <p>한번 삭제한 프로젝트는 다시 되돌릴 수 없습니다.</p>
+      <p>그래도 정말 삭제하시겠습니까?</p>
+      <div className="btn del-btn">삭제</div>
+    </div>
+  );
 };
 
 export default ProjSetDelete;
