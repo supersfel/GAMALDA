71e95eabe99e77cfe46533acd01dd997fca134a6
정민규 <jmg081005@naver.com>
2023-05-16 17:17:55 +0900

    feat(fe): private 설정 퍼블리싱

--- fe/src/components/projectSet/ProjSetPrivate.tsx
+++ fe/src/components/projectSet/ProjSetPrivate.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProjSetPrivate = () => {
-  return <div>private</div>;
+  const [isPrivate, setIsPrivate] = useState(false);
+
+  const handleToggle = () => {
+    setIsPrivate(!isPrivate);
+  };
+
+  return (
+    <div className="proj-set-private">
+      <div className="title">
+        <p>Private 설정</p>
+        <div className="toggle-btn">
+          <label>
+            <input
+              role="switch"
+              type="checkbox"
+              className="prvate-btn"
+              checked={isPrivate}
+              onChange={handleToggle}
+            />
+          </label>
+        </div>
+      </div>
+
+      <p>팀원이 아닌 다른 사람들이 프로젝트를 볼 수 없습니다</p>
+    </div>
+  );
 };
 
 export default ProjSetPrivate;
