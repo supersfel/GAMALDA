2e324d39df7a31d730a02ba92c19a9b8b6e61815
정민규 <jmg081005@naver.com>
2023-05-16 19:24:45 +0900

    feat(fe): 이미지 미리보기 기능 추가

--- fe/src/components/projectSet/ProjSetInfo.tsx
+++ fe/src/components/projectSet/ProjSetInfo.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { ReactComponent as UploadSVG } from 'assets/svg/upload.svg';
 
 const ProjSetInfo = () => {
+  const [imageSrc, setImageSrc]: any = useState(
+    'https://picsum.photos/300/300',
+  );
+
+  const onUpload = (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result || null); // 파일의 컨텐츠
+        resolve();
+      };
+    });
+  };
+
   return (
     <form className="proj-set-info">
       <div className="name">
@@ -12,14 +29,19 @@ const ProjSetInfo = () => {
         <p>프로젝트 섬네일</p>
         {/* 이미지는 후에 다시 작업 */}
         <div className="container">
-          <img src="https://picsum.photos/300/300" alt="" />
+          <img src={imageSrc} alt="" />
           <label htmlFor="proj-img-file">
             <div className="btn-upload btn">
               <UploadSVG />
               업로드
             </div>
           </label>
-          <input type="file" name="file" id="proj-img-file" />
+          <input
+            type="file"
+            name="file"
+            id="proj-img-file"
+            onChange={onUpload}
+          />
         </div>
 
         <button className="btn success">완료</button>
