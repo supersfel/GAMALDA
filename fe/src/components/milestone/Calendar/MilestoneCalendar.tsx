02308f5e24ab4827819186879c5f892b96a07479
Gotkwondo <dhwngus99@gmail.com>
2023-07-18 16:19:22 +0900

    refactor(fe): 코드 리뷰 적용, 불필요한 코드 삭제, react key 이슈 수정

--- fe/src/components/milestone/Calendar/MilestoneCalendar.tsx
+++ fe/src/components/milestone/Calendar/MilestoneCalendar.tsx
@@ -5,85 +5,35 @@
 import ContextMenuInCalendar from 'components/modules/Modal/Milestone/ContextMenuInCalendar';
 import { RootState } from 'modules/index';
 import { setModal } from 'modules/modal';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { MILESTONEVAL } from 'utils/milestone';
-
-import { dateTostr, getDateByDiff, isPastDate } from 'utils/time';
-
-import MilestoneBlock from '../MilestoneBlock';
 import { blockInfoType } from '../type';
-import { setDayCnt } from 'modules/projectSetting';
-import { useParams } from 'react-router-dom';
 import { ThunkDispatch } from 'redux-thunk';
 import CalendarHeader from './CalendarHeader';
 
-import { format, addMonths, subMonths, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays } from 'date-fns';
+import { addMonths, subMonths, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays } from 'date-fns';
 import CalendarCell from './CalendarCell';
 
 // dark모드 여부
 interface Props {
   isColorBlack: boolean;
-  setClickDate: React.Dispatch<React.SetStateAction<Date>>;
-  setClickBlock: React.Dispatch<
-    React.SetStateAction<blockInfoType | undefined>
-  >;
-}
-// 
-interface curDateListType {
-  date: string;
-  cnt: number;
-  accCnt: number;
-}
-
-interface posType {
-  curLeft: number;
-  pastLeft: number;
-  start: number;
 }
 
 const MilestoneCalendar = ({
   isColorBlack,
-  setClickDate,
-  setClickBlock,
 }: Props) => {
   // block들의 정보
   const blockInfo = useSelector((state: RootState) => state.milestoneBlock).map(e =>e)
   const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
   const gridRef = useRef<HTMLDivElement>(null);
-  const projectId = useParams().projectId as string;
 
-  // 블럭 정보 list 배열
-  const [blockList, setBlockList] = useState<blockInfoType[]>([]);
-
-  // const [startDay, setStartDay] = useState<Date>(new Date());
-  // const [monthCnt, setMonthCnt] = useState(2);
-
-  /**
-   * curDayList는 일별로 정리된 배열
-   */
-  const [curDayList, setCurDayList] = useState<Date[]>([]);
-  // const [dayPosMap, setDayPosMap] = useState<Map<string, string>>(new Map()); // 날짜, 요일 배열
-
-  // const [curMonthList, setCurMonthList] = useState<curDateListType[]>([]);
-  // const [curYearList, setCurYearList] = useState<curDateListType[]>([]);
-  const test = blockInfo.filter((e) => {
-  
-})
   // 날짜, 요일 관리
   const [currentMonth, setCurrentMonth] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const [rightClickPos, setRightClickPos] = useState<number[]>([0, 0]);
   const openModal = useSelector((state: RootState) => state.modal);
-  const projectSet = useSelector((state: RootState) => state.projectSetting);
-
-  useEffect(() => {
-    setCurDayList(initialCurDayList(currentMonth));
-    setBlockList(validProjects(currentMonth, blockInfo));
-  }, [currentMonth]);
-// console.log(blockList)
   
   /* 우클릭 이벤트 */
   const handleContextMenu = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -109,29 +59,6 @@ const MilestoneCalendar = ({
   }
 
   /**
-   * 달력에 표시되는 날짜들을 생성해주는 함수
-   * @param currentMonth 
-   * @returns 해당 달의 달력에 표시되는 날짜 리스트 배열
-   */
-  const initialCurDayList = (currentMonth: Date) => {
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
-
-    let days = [];
-    let startDay = startDate;
-    let formattedDate = '';
-
-    while (startDay <= endDate) {
-      formattedDate = format(startDay, 'yy-MM-d');
-      days.push(startDay)
-      startDay = addDays(startDay, 1);
-    }
-    return days;
-  }
-
-  /**
    * 현제 달력에 표시할 블럭들의 정보 배열을 추려주는 함수
    * @param currentMonth 
    * @param arr 
@@ -145,7 +72,6 @@ const MilestoneCalendar = ({
     const test = arr.filter((e) => new Date(e.end) < endDate && new Date(e.start) > startDate)
     return test;
   }
-  // console.log(validProjects(currentMonth, blockInfo));
 
   /**
    * 요일별 문자을 div태그 작업 해줌
