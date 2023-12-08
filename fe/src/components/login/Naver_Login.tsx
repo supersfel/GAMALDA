d2b8ebcf2f26a0a449cca426f1432f0c5a87eb20
Gotkwondo <dhwngus99@gmail.com>
2023-12-08 16:34:33 +0900

    ♻️ refactor(fe): 1)눈 내리는 애니메이션의 눈 생성 함수에서 의존성과 부수 효과 정리. 2)브라우저 width와 height를 변경시 발생하는 애니메이션 수정

--- fe/src/components/login/Naver_Login.tsx
+++ fe/src/components/login/Naver_Login.tsx
@@ -1,48 +1,63 @@
 import { ReactComponent as GamaldaSVG } from 'assets/svg/gamaldalogo.svg';
 import naverLogin from 'assets/png/naver_login.png';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav_Login = () => {
   //  process.env.REACT_APP_NAVER_CLIENT_ID 는 네이버에서 생성된 client ID, process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL 는 네이버에서 설정한 콜백 url이다.
   let naver_api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${encodeURI(process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL!)}&state=${process.env.REACT_APP_NAVER_STATE}`;
-  const snowEffetRef = useRef<HTMLDivElement>(null);
-
-  //  추후 분리 예정
-  //  로그인 페이지 배경에 눈을 생성해주는 함수
-  const getParticles = (particleAreaRef: React.RefObject<HTMLElement>) => {
-    const particles = particleAreaRef;
+  const [particles, setParticles] = useState<Array<JSX.Element>>([]);
+  
+  //  로그인 페이지 배경에 눈을 생성해주는 함수(❗액션 분리 필요 : setParticles❗)
+  const getParticles = () => {
     const border = ["50%", "0%"];
     const colors = ["#FF6B6B", "#FFE66D", "#ffffff"];
+    let w = document.documentElement.clientWidth;
+    let h = document.documentElement.clientHeight;
 
-    if (particles === null) return;
-    var np = document.documentElement.clientWidth / 40;
-    for (var i = 0; i < np; i++) {
-      var w = document.documentElement.clientWidth;
-      var h = document.documentElement.clientHeight;
-      var rndw = Math.floor(Math.random() * w) + 1;
-      var rndh = Math.floor(Math.random() * h) + 1;
-      var widthpt = Math.floor(Math.random() * 8) + 5;
-      var opty = Math.floor(Math.random() * 4) + 1;
-      var anima = Math.floor(Math.random() * 12) + 8;
-      var bdr = Math.floor(Math.random() * 2);
-      var color = Math.floor(Math.random() * 3);
-      var div = document.createElement("div");
-      div.style.position = "absolute";
-      div.style.marginLeft = rndw + "px";
-      div.style.marginTop = rndh + "px";
-      div.style.width = widthpt + "px";
-      div.style.height = widthpt + "px";
-      div.style.opacity = opty + "";
-      div.style.backgroundColor = colors[color];
-      div.style.borderRadius = border[bdr];
-      div.style.animation = "move " + anima + "s ease-in infinite";
-      particles.current!.appendChild(div);
+    // 눈 애니메이션에서 눈들의 위치 정보를 바탕으로 생성한 태그를 담은 배열을 반환해주는 함수
+    const createDivForSnowPosition = (width: number, height: number): JSX.Element[] => {
+      const particlesArray: JSX.Element[] = [];
+
+      for (let i = 0; i < width / 40; i++) {
+        particlesArray.push(
+          <div>
+            <div
+              key={i}
+              style={{
+                position: "absolute",
+                marginLeft: Math.floor(Math.random() * width) + 1 + "px",
+                marginTop: Math.floor(Math.random() * height) + 1 + "px",
+                width: Math.floor(Math.random() * 8) + 5 + "px",
+                height: Math.floor(Math.random() * 8) + 5 + "px",
+                opacity: Math.floor(Math.random() * 4) + 1 + "",
+                backgroundColor: colors[Math.floor(Math.random() * 3)],
+                borderRadius: border[Math.floor(Math.random() * 2)],
+                animation: `move ${Math.floor(Math.random() * 12) + 8}s ease-in infinite`,
+              }}
+            ></div>
+          </div>
+        );
+      }
+      return particlesArray;
     }
+
+    setParticles(createDivForSnowPosition(w, h));
   };
+
   useEffect(() => {
-    getParticles(snowEffetRef);
+    getParticles();
+    
+    // 사용자가 브라우저의 크기를 변경시 변경된 공간에 애니메이션을 나타내기 위해 addEventListener의 resize 추가
+    // 마운트 될 때 이벤트 리스너를 더하고, 언마운트 될 때 제거해준다. 
+    // 참고 링크 : https://db2dev.tistory.com/entry/React-resize-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EB%8B%A4%EB%A3%A8%EA%B8%B0
+    window.addEventListener('resize', getParticles);
+    return () => {
+      // cleanUp
+      window.removeEventListener('resize', getParticles);
+    };
   }, [])
+
   return (
     <>
       <div className="bg"></div>
@@ -61,7 +76,7 @@ const Nav_Login = () => {
           </div>
         </div>
       </div>
-      <div className="particles" ref={snowEffetRef} />
+      <div className="particles">{particles}</div>
     </>
   )
 };
