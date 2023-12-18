1cfca086e3640b23b8464195ae732df8d58d1c24
오주현 <dhwngus99@gmail.com>
2023-12-18 16:24:58 +0900

    ♻️ refactor(fe): 로그인 주소 로직 분리 및 env활용

--- fe/src/hooks/generateNaverApiUrl.ts
+++ fe/src/hooks/generateNaverApiUrl.ts
@@ -1,4 +1,6 @@
-//  process.env.REACT_APP_NAVER_CLIENT_ID 는 네이버에서 생성된 client ID, process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL 는 네이버에서 설정한 콜백 url이다.
+/**
+ * @returns 네이버에 로그인 요청을 보내는 URL
+ */
 export const generateNaverApiUrl = (): string => {
   return `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${encodeURI(process.env.REACT_APP_NAVER_LOGIN_CALLBACK_URL!)}&state=${process.env.REACT_APP_NAVER_STATE}`;
 }
